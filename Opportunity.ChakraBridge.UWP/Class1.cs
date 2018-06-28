9a1dcddc7ab3200bcbdb578c3a905b7e43d13e47
OpportunityLiu <opportunity@live.in>
2018-06-28 11:06:12 +0800

    添加项目文件。

---
+++ Opportunity.ChakraBridge.UWP/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opportunity.ChakraBridge.UWP
+{
+    public sealed class Class1
+    {
+    }
+}
